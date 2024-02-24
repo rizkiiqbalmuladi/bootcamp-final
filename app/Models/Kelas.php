e7109d62bfa39509e49f45006fb56131eea9d032
Farhan Hamdallah <fhbluebeast356@gmail.com>
2024-02-24 11:27:57 +0700

    make Kelas to be not guarded

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Kelas extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
     public function users()
     {
         return $this->hasMany(User::class);
