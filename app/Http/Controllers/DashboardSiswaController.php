a470feebdc7d31b854408ebd020698d2de1394b0
RizkiIqbal02 <roxashiqbal12@gmail.com>
2024-02-27 19:31:14 +0700

    Menambahkan UserService kedalam DashboardSiswaController

--- app/Http/Controllers/DashboardSiswaController.php
+++ app/Http/Controllers/DashboardSiswaController.php
@@ -5,18 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use App\Models\User;
 use App\Models\Kelas;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class DashboardSiswaController extends Controller
 {
     public $role_id;
-    public function __construct()
+    protected $userService;
+    public function __construct(UserService $userService)
     {
         $this->role_id = 3;
+        $this->userService = $userService;
     }
     public function index()
     {
-        $user = User::with(['role', 'kelas'])->where('role_id', 'like', $this->role_id)->get();
+        $user = $this->userService->getUserByRoleId($this->role_id);
         return view('dashboard.guru.index', ['user' => $user]);
     }
 }
